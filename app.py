a0dea724a4e1b438638158e85fb1c65886ab9110
dias11 <koliopanos@yahoo.com>
2022-12-29 22:51:48 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,11 +3,12 @@ import pandas as pd
 import numpy as np
 
 #from matplotlib import cm  #%matplotlib inline
-
 import matplotlib.pyplot as plt
+
+
 import matplotlib as mpl
 import matplotlib.colors as colors
-mygreens = colors.LinearSegmentedColormap.from_list('mygreens', ['#000000', '#00FF00'])
+mycolor = colors.LinearSegmentedColormap.from_list('mycolor', ['#000B65','#019EE2','#E2E201', '#EA0B00'])
 
 import pykrige.kriging_tools as kt
 from pykrige.ok import OrdinaryKriging
@@ -17,8 +18,6 @@ st.title('All interpolations')
 st.text("This is a web app to explore interpolations from IRMA's Data")
 uploaded_file = st.file_uploader('Upload your file here')
 
-x_real = np.array([39.09933, 39.07888, 39.14904, 39.12208, 39.09518, 39.05061, 39.21634, 39.25915, 39.21899])
-y_real = np.array([20.73085, 20.88525, 20.87591, 20.94730, 21.06071, 21.01207, 20.91295, 20.78422, 20.82493])
 
 if uploaded_file:
 #    st.header('data statistics')
@@ -34,25 +33,32 @@ if uploaded_file:
     data = csv_data.to_numpy()   # Convert csv dataframe to numpy array
 #    st.text(data)                #Print data numpy array
 
-    x = data[:, 1]              # x coordinates 
-    st.text(x)
+    x = data[:, 2]              # x coordinates 
+#    st.text(x)
 
-    y = data[:, 2]              # y coordinates
-    st.text(y)
+    y = data[:, 1]              # y coordinates
+#    st.text(y)
 
     z = data[:, 5]
-    st.text(z)
+#    st.text(z)
     
     
-    fig1 = plt.figure()
-    ax1 = fig1.add_subplot()
-    ax1.set_title('The output of st.pyplot()')
-    surf = ax1.scatter(y, x, 100, z, cmap=mygreens)
-    fig1.colorbar(surf)
-    st.pyplot(fig1)
+#   This Part is the plotter of Weather Stations
+
     
     
+    fig_WS,axis_WS = plt.subplots()
+    axis_WS.set_title('Wheather Stations Locations')
+    axis_WS.set_xlabel('x')
+    axis_WS.set_ylabel('y')
+    axis_WS.set_xlim(20.63, 21.15)
+    axis_WS.set_ylim(39.00, 39.33)
+    WSG = axis_WS.scatter(x, y, 100, z, cmap=mycolor, edgecolors='black')
+    fig_WS.colorbar(WSG)
+    st.write(fig_WS)
     
+    
+#   This Part is the plotter of Kriging Exponential Interpolation    
     
     
     ok = OrdinaryKriging(
@@ -64,19 +70,76 @@ if uploaded_file:
     enable_plotting = True
     )
     
-    gridx = np.linspace(39.05, 39.26, 100)
-    gridy = np.linspace(20.73, 21.10 ,100)
+    gridx = np.linspace(20.63, 21.15, 100)
+    gridy = np.linspace(39.00, 39.33 ,100)
     zstar, ss = ok.execute('grid', gridx, gridy )
- 
-    st.text(gridx)
-    st.text(gridy)
-    st.text(zstar)
+#-----------end of calculations   
     
-    
-    fig2 = plt.figure()
-    ax2 = fig2.add_subplot()
-    ax2.set_title('The output of st.pyplot()')
+    fig_KR_exp,axis_KR_exp = plt.subplots()
+    axis_KR_exp.set_title('Kriging Exponential')
+    axis_KR_exp.set_xlabel('x')
+    axis_KR_exp.set_ylabel('y')
+    axis_KR_exp.set_xlim(20.63, 21.15)
+    axis_KR_exp.set_ylim(39.00, 39.33)
+    axis_KR_exp.contourf(gridx, gridy,  zstar,100, cmap=mycolor)
+    KR_exp = axis_KR_exp.scatter(x, y, 100, z, cmap=mycolor, edgecolors='black')
+    fig_KR_exp.colorbar(KR_exp)
+    st.write(fig_KR_exp)
 
-    surf2 = ax2.contourf(gridy, gridx,  zstar,100, cmap=mygreens)
-    fig2.colorbar(surf2)
-    st.pyplot(fig2)
+    
+#   This Part is the plotter of Kriging Gaussian Interpolation  
+
+    ok = OrdinaryKriging(
+    x,
+    y,
+    z,
+    variogram_model = 'gaussian', #exponential, gaussian, linear
+    verbose = True,
+    enable_plotting = True
+    )
+    
+    gridx = np.linspace(20.63, 21.15, 100)
+    gridy = np.linspace(39.00, 39.33 ,100)
+    zstar, ss = ok.execute('grid', gridx, gridy )
+
+
+    fig_KR_gau,axis_KR_gau = plt.subplots()
+    axis_KR_gau.set_title('Kriging Gaussian')
+    axis_KR_gau.set_xlabel('x')
+    axis_KR_gau.set_ylabel('y')
+    axis_KR_gau.set_xlim(20.63, 21.15)
+    axis_KR_gau.set_ylim(39.00, 39.33)
+    axis_KR_gau.contourf(gridx, gridy,  zstar,100, cmap=mycolor)
+    KR_gau = axis_KR_gau.scatter(x, y, 100, z, cmap=mycolor, edgecolors='black')
+    fig_KR_gau.colorbar(KR_gau)
+    st.write(fig_KR_gau)
+
+
+#   This Part is the plotter of Kriging linear Interpolation 
+
+    ok = OrdinaryKriging(
+    x,
+    y,
+    z,
+    variogram_model = 'linear', #exponential, gaussian, linear
+    verbose = True,
+    enable_plotting = True
+    )
+    
+    gridx = np.linspace(20.63, 21.15, 100)
+    gridy = np.linspace(39.00, 39.33 ,100)
+    zstar, ss = ok.execute('grid', gridx, gridy )
+
+
+    fig_KR_lin,axis_KR_lin = plt.subplots()
+    axis_KR_lin.set_title('Kriging Linear')
+    axis_KR_lin.set_xlabel('x')
+    axis_KR_lin.set_ylabel('y')
+    axis_KR_lin.set_xlim(20.63, 21.15)
+    axis_KR_lin.set_ylim(39.00, 39.33)
+    axis_KR_lin.contourf(gridx, gridy,  zstar,100, cmap=mycolor)
+    KR_lin = axis_KR_lin.scatter(x, y, 100, z, cmap=mycolor, edgecolors='black')
+    fig_KR_lin.colorbar(KR_lin)
+    st.write(fig_KR_lin)
+
+
